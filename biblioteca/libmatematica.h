58eab18425a8a5e252c8598b565505257f9472eb
Bernardocoding <128652568+Bernardocoding@users.noreply.github.com>
2024-03-13 16:01:21 -0300

    comentarios

--- biblioteca/libmatematica.h
+++ biblioteca/libmatematica.h
@@ -1,3 +1,19 @@
-float calcular_seno(float angulo);// calcula o seno
-float calcular_cosseno(float angulo);//calcula o cosseno
-float calcular_tangente(float angulo);//calcula a tangente
+
+/**
+ @param angulo medida do angulo em radiano
+ @return o seno do angulo
+ 
+ **/
+float calcular_seno(float angulo);
+/**
+ @param angulo medida do angulo em radiano
+ @return o cosseno do angulo
+ 
+ **/
+float calcular_cosseno(float angulo);
+/**
+ @param angulo medida do angulo em radiano
+ @return o tangente do angulo
+ 
+ **/
+float calcular_tangente(float angulo);
